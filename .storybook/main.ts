a165e38d6a6c1bd883aec43c0e57e33445d02562
Vlad Budica <vladbudica@Vlads-MacBook-Pro.local>
2025-11-14 15:07:37 +0200

    full app

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,18 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+ stories: ['../src/components/**/*.stories.@(ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
-    "@storybook/addon-docs",
-    "@storybook/addon-vitest",
-    "@chromatic-com/storybook"
+    '@storybook/addon-docs',
+    '@storybook/addon-vitest',
+    '@chromatic-com/storybook',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
 };
+
 export default config;
