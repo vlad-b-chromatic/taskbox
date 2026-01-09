a1862e449ea0c9ccc7f9182e766d324a80a5573f
Vlad Budica <vladbudica@Vlads-MacBook-Pro.local>
2026-01-09 16:38:28 +0200

    changed background to red

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -40,7 +40,7 @@ export default function Task({
           name="title"
           id={`title-${id}`}
           placeholder="Input title"
-         style={{ backgroundColor: 'white' }}
+         style={{ backgroundColor: 'red' }}
         />
       </label>
       {state !== "TASK_ARCHIVED" && (
